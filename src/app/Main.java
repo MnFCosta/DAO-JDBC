6d783bb20ba3a547892c361e4ce84d26c9ca928f
Manoel Costa <mnfcosta1@gmail.com>
2026-04-09 13:01:36 -0300

    Criando DAOs e daofactory

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,7 @@
 package app;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,6 +11,8 @@ public class Main {
     static void main() {
         Department dp = new Department(1, "Books");
         Seller seller = new Seller(1, "Manoel Costa", "manoel@gmail.com", new Date(), 3000.0, dp);
+
+        SellerDAO sellerDAO = DaoFactory.createSellerDao();
 
         System.out.println(dp);
         System.out.println(seller);
---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,10 @@
+package model.dao;
+
+import model.dao.impl.SellerDAOJDBC;
+
+public class DaoFactory {
+
+    public static SellerDAO createSellerDao(){
+        return new SellerDAOJDBC();
+    }
+}
