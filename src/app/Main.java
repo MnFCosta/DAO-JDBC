795c31d7aa04c5eef22250c872071e7ac5f75a2f
Manoel Costa <mnfcosta1@gmail.com>
2026-04-09 12:05:39 -0300

    Adicionando a classe entidade Department

---
+++ src/app/Main.java
@@ -0,0 +1,11 @@
+package app;
+
+import model.entities.Department;
+
+public class Main {
+    static void main() {
+        Department dp = new Department(1, "Books");
+
+        System.out.println(dp);
+    }
+}
