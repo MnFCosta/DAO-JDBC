6836da0fe629f3bd29136972c800e33d7e8452ce
Manoel Costa <mnfcosta1@gmail.com>
2026-04-09 12:25:38 -0300

    Adicionando entity Seller

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,11 +1,16 @@
 package app;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     static void main() {
         Department dp = new Department(1, "Books");
+        Seller seller = new Seller(1, "Manoel Costa", "manoel@gmail.com", new Date(), 3000.0, dp);
 
         System.out.println(dp);
+        System.out.println(seller);
     }
 }
